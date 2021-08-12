865191a6a05a6ea38aa00a2333c4640e8c8e529a
Matheus da Silva Porto <MPorto1994@gmail.com>
2021-08-12 14:29:38 -0300

    retirada de comentários inuteis

--- onlyscript.js
+++ onlyscript.js
@@ -1,16 +1,13 @@
 function nomeMaisApropriado(n) {
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
     let x=[];
     let y=[];
     for (let l = 0; l <= n; l++) {
 
         for (let c = 0; c <= n; c++) {
-            //y[c] = l porque neste caso a tabela fica com todos elementos = 10????
-            //porque se somo l+c ele concatena?Porque l=10?
             y[c]=l*c
         }
         x[l]=y;
-        y=[]
+        y=[]       //inicialmente não tinha essa linha, e não entendi porque ela se faz necessária, apesar de ter resolvido o problema(l=10)
     }
     console.table(x)
 }
